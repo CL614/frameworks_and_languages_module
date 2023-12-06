7afb797bcd5ec6cc1e42079feae3ab6b2ca0b287
CL614 <114404617+CL614@users.noreply.github.com>
2023-12-06 10:09:35 +0000

    changes made to post request

--- server/app.js
+++ server/app.js
@@ -12,56 +12,67 @@ app.use(express.json())
 // sets up middleware for cors
 app.use(cors())
 //sets up middleware for handling URL payloads
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 // creates welcome message
 app.get('/', (req, res) => {
-res.send("Hello World");
- 
+  res.send("Hello World");
+
 });
 //defines path to get current date
 const currentDate = new Date().toISOString()
 
 // initializes item with items details
 let ITEMS = {
-  1: {
-    "id": 1,
-    "keywords": ["saw","wood","tools"],
-    "description": "A saw and wood ",
-    "image":"https://placekitten.com/200/300",
-    "latitude": 37.38797546109132, 
-    "longitude": -122.05688209785687,
-    "date_start": currentDate,
-    "date_end": currentDate,
-
-  },
+  "id": 1,
+  "keywords": ["saw", "wood", "tools"],
+  "description": "A saw and wood ",
+  "image": "https://placekitten.com/200/300",
+  "latitude": 37.38797546109132,
+  "longitude": -122.05688209785687,
+  "date_start": currentDate,
+  "date_end": currentDate,
 };
 
 
 app.get('/items', (req, res) => {
- res.json(ITEMS)
+  res.json(ITEMS)
 })
 
 app.post('/item', (req, res) => {
-  const reqFields = ["user_id", "keywords", "description", "lat" , "lon"]
-  if (!reqFields.every(field=>req.body.hasOwnPropert(field)))
-  {
-    console.log("missing Data")
+
+  if (!req.body.user_id || !req.body.keywords || !req.body.description || !req.body.lat || !req.body.lon) {
+    console.log("Missing Data")
     return res.status(405).json()
   }
-  else
-  {
-  let new_key = Math.random()
-  //ITEMS = { new_key :  }
-  console.log(ITEMS)
-  res.status(201).json()
-  }
- })
+  else {
+    req.body['date_start'] = currentDate
+    req.body['date_end'] = currentDate
+    req.body['item_id'] = Math.random()
+    console.log("About to add item")
 
- app.delete('/item:item_id', (req, res) => {
+    let new_Item = {
+      "id": Math.random(),
+      "user_ID": req.body.user_id,
+      "keywords": req.body.keywords,
+      "description": req.body.description,
+      "image": req.body.image,
+      "latitude": req.body.lat,
+      "longitude": req.body.lon,
+      "date_start": currentDate,
+      "date_end": currentDate,
+    };
+
+    ITEMS.push(new_Item)
+    console.log("successful Post")
+    res.status(201).json(new_Item)
+  }
+})
+
+app.delete('/item:item_id', (req, res) => {
   //remove using delete and id from dictionary
   res.status(204).json()
- })
+})
 
 
 
