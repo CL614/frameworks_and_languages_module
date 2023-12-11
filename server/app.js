a23bb26024e4accce613ebac9fe3e4da53ad8eb3
CL614 <114404617+CL614@users.noreply.github.com>
2023-12-11 10:33:23 +0000

    all refrenceses to item_id changed to match spec

--- server/app.js
+++ server/app.js
@@ -16,8 +16,8 @@ app.use(express.urlencoded({ extended: true }))
 
 // loads client.html when server is loaded
 app.get('/', (req, res) => {
-  //res.send("Hello World");
-  res.status(200).sendFile("client.html", {root: __dirname})
+//res.send("Hello World");
+res.status(200).sendFile("client.html", { root: __dirname })
 
 });
 //defines path to get current date
@@ -28,14 +28,14 @@ let randomId = Math.random();
 // initializes item with items details
 let ITEMS = [
   {
-    "item_id": randomId,
+    "id": randomId,
     "user_id": "Callum123",
     "keywords": [
       "Word1",
       "Word2",
       "Word3",
     ],
-    "description": "1111111111111111",
+    "description": "a hammer and nails set",
     "image": "https://i.imgur.com/SCEwQdk.jpeg",
     "lat": 12.86568,
     "lon": -67.09876543,
@@ -52,52 +52,54 @@ app.get('/items', (req, res) => {
 
 app.post('/item', (req, res) => {
 
-  let reqFields = ['user_id', 'keywords', 'description', 'image', 'lat', 'lon' ]
+  let reqFields = ['user_id', 'keywords', 'description', 'image', 'lat', 'lon']
   const enteredFields = Object.keys(req.body).toString().split(',')
 
   let expectedFields = 5;
   let fields = 0;
 
-  if(enteredFields[3] === 'image' || enteredFields[4] === 'image'){
+  if (enteredFields[3] === 'image' || enteredFields[4] === 'image') {
     expectedFields++;
   }
-  else{
-    reqFields.splice(3,1);
+  else {
+    reqFields.splice(3, 1);
   }
-  for (let i = 0; i < reqFields.length; i++){
-    if (enteredFields[i] == reqFields[i]){
+  for (let i = 0; i < reqFields.length; i++) {
+    if (enteredFields[i] == reqFields[i]) {
       fields++;
     }
   }
-   if (fields !== expectedFields){
+  if (fields !== expectedFields) {
     console.log("Missing Data! 405")
-    res.status(405).json({"message": "Missing data"})
-   }
-  
-   let new_Item = {
-    item_id: randomId,
+    res.status(405).json({ "message": "Missing data" })
+  }
+  else {
+  let new_Item = {
+    id: randomId,
     ...req.body,
     date_from: currentDate,
     date_to: currentDate
-   }
-    
-    ITEMS.push(new_Item)
-    console.log("successful Post")
-    res.status(201).json(new_Item)
-  
+  }
+
+  ITEMS.push(new_Item)
+  console.log("Successful Post")
+  res.status(201).json(new_Item)
+  console.log(ITEMS)
+  console.log(new_Item)
+}
+
 })
 
-app.delete('/item:item_id', (req, res) => {
-  const ID = (req.params.item_id)
+app.delete('/item:id', (req, res) => {
+  
 
-  if (ID === -1)
-  {
-    return res.status(404).json({"message": "ID does not exist"})
+  if (req.params.id === -1) {
+    return res.status(404).json({ "message": "ID does not exist" })
   }
   ITEMS.splice(ID, 1)
   console.log("DELETE 204 id: ", ID.toString())
   res.status(204).json()
-  
+
 })
 
 
